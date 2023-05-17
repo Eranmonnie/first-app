9fdf124c3c8f41c2c37373b5244011befc6e5425
Eranmonnie <ajalaferanmi123@gmail.com>
2023-05-17 10:06:29 +0100

    Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,8 +10,15 @@ class Post extends Model
     use HasFactory;
     // protected $guarded = ['id']; the oposite of fillable
     protected $fillable = ['title', 'sippet', 'body', 'link'];
-    
+
     // public function getRouteKeyName(){
     //     return 'slug';
     // } specify the data to be searched by from url 
+
+    //specification of relationships 
+    //each post should belong to only one category and can only have one foreign key to it 
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    
 }
