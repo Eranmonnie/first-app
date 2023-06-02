ee9f664478fcaf3a1e07a1df05a3b03cf720bbbe
Eranmonnie <ajalaferanmi123@gmail.com>
2023-06-02 09:28:32 +0100

    comment section

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,28 @@
+
+    <article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60" alt="profile picture" width="60" height="60" class="rounded-xl">
+        </div>
+
+        <div>
+            
+            <header class="mb-4">
+
+                <h3 class="font-bold">
+                     John doe
+                </h3>
+
+                <p class="text-xs"> 
+                    <time>Posted 8 months ago</time> 
+                </p>
+
+            </header>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+             dolores culpa amet beatae vitae sunt excepturi exercitationem volu
+            soluta officia numquam exercitationem in voluptate, fugiat quos id.</p>
+
+        </div>
+
+    </article>
