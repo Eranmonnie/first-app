e0af316808f19c84234b9149dbee40f083df3fa0
Eranmonnie <ajalaferanmi123@gmail.com>
2023-06-04 13:10:31 +0100

    comment form style

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -50,8 +50,28 @@
             </div>
 
           
+            
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <x-pannel>
+                <form action="POST" class="#">
+
+                    <header class="flex items-center">
+                        <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="profile picture" width="40" height="40" class="rounded-full">
+                        <h2 class="ml-3"> Want to participate?</h2>
+                    </header>
+
+                    <div class="mt-6">
+                        <textarea name="body"rows="5" class="w-full text-sm focus: outline-none focus:ring" placeholder="comment here!!"></textarea>
+                    </div>
+
+                    <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                        <button type="submit" class="bg-blue-500 text-white uppercase rounded-full text-xs font-semibold py-2 px-10 hover:bg-blue-600">Post</button>
+                    </div>
+ 
+                </form>
+                </x-pannel>
+
                 @foreach ($post->comment as $comment)
 
                 <x-post-comment :comment="$comment"/>
